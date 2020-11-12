b1b6cc4607c584e8719e819caa074078c626bab4
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2020-11-11 21:31:14 -0300

    Clase 2: Introducciona componentes y estilos

--- miAppNative/App.js
+++ miAppNative/App.js
@@ -11,104 +11,173 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  FlatList,
   View,
   Text,
   StatusBar,
+  Button,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  TouchableHighlight, 
+  Image,
+  Dimensions,
+  ImageBackground,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const {height, width} = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  image:{
+    borderRadius: 100,
+    width: width * 0.4,
+    height: 200,
+  }
+});
 
 const App: () => React$Node = () => {
+
+  console.log({height, width})
+  //enhanced object literal
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View 
+            style={{
+              backgroundColor: '#2ecc71',
+              padding: 20,
+              margin: 5,
+              borderRadius: 10,
+              borderColor: 'orange',
+              borderWidth: 1,
+             }}>
+            <Text 
+              style={{
+                color: 'white',
+                fontSize: 30,
+                textDecorationLine: 'underline line-through'
+              }}>WELCOME TO VEGAN LIFE</Text>
           </View>
+          <Button color='#f1c40f' title='CALENTAMIENTO GLOBAL'/>
+          <Button color='red' title='INCENDIOs FORESTALES'/>
+          <Button onPress={ () => {
+            console.log("boton incendios")
+          } } color='blue' title='GLACIARES EN PELIGRO'/>
+          <Button color='green' title='DESFORESTACION'/>
+
+          <TouchableOpacity
+            style={{
+              backgroundColor: "green",
+              padding: 20,
+              margin: 5,
+              borderRadius: 10,
+              alignItems: "center",
+              alignSelf: "center"
+            }}
+            onPress={ () => {
+              console.log("boton onpress")
+            } }
+            onLongPress={ () => {
+              console.log("boton presionado")
+            } }
+          >
+            <Text style={{
+              color: "white",
+              fontSize: 30,
+              textDecorationLine: 'underline line-through'
+            }}>Button Touch</Text>
+          </TouchableOpacity>
+
+          <TouchableHighlight
+            style={{
+              backgroundColor: "green",
+              padding: 20,
+              margin: 5,
+              borderRadius: 10,
+              alignItems: "center",
+              alignSelf: "center"
+            }}
+            onPress={ () => {
+              console.log("boton onpress")
+            } }
+            onLongPress={ () => {
+              console.log("boton presionado")
+            } }
+            underlayColor="#2ecc71"
+          >
+            <Text style={{
+              color: "white",
+              fontSize: 30,
+              textDecorationLine: 'underline line-through'
+            }}>Button Touch High Light</Text>
+          </TouchableHighlight>
+
+          <View style={{
+            alignItems: "center"
+          }}>
+            <Image
+              style={styles.image}
+              resizeMode= "cover"
+              source={{uri: 
+                "https://images.pexels.com/photos/3626733/pexels-photo-3626733.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              }}
+            />
+            <Image
+              style={styles.image}
+              resizeMode= "cover"
+              source={{uri: 
+                "https://images.pexels.com/photos/3626733/pexels-photo-3626733.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              }}
+            />
+            <Image
+              style={styles.image}
+              resizeMode= "cover"
+              source={{uri: 
+                "https://images.pexels.com/photos/3626733/pexels-photo-3626733.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              }}
+            />
+
+          <ImageBackground 
+            style={{
+              height: 200,
+              width: 200,
+              alignItems: "flex-start",
+            }}
+            source={{uri: 
+            "https://images.pexels.com/photos/4508024/pexels-photo-4508024.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            }}>
+            <Text style={{
+              backgroundColor: "gray",
+            }}>Componente View con una imagend de fondo</Text>
+          </ImageBackground>
+
+          </View>
+
+
         </ScrollView>
+        
+        
+        <FlatList
+          data={[
+            {id: 1, nombre: "Bla"}, 
+            {id: 2, nombre: "Fu"},
+            {id: 3, nombre: "Fa"},
+          ]}
+          
+          keyExtractor={(item) => item.id.toString()}
+
+          renderItem={({item: {id, nombre}}) => {
+            return (
+              <View>
+              <Text>Hola {nombre}</Text>
+              </View>
+            )
+          }}
+        />
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
